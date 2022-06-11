697c3d35600d9bb645c04a1ca3390e4ae546c08b
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-12 01:19:43 +0200

    Create automatic-node-creation1.sh

---
+++ automatic-node-creation1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+mkdir -p /var/jenkins_home/jars/
+
+if [ ! -f /var/jenkins_home/jars/jenkins-cli.jar ]
+
+then
+    
+    tee /var/jenkins_home/ansible/add-node.yml <<EOF
+---
+- name: bootstrap
+  become: true
+  hosts: localhost
+  task:
+    - name: Download jenkins-cli.jar file
+      get_url:
+        url: $1
+        dest: /var/jenkins_home/jars/jenkins-cli.jar
+        mode: '0777'
+EOF
+
+fi
