799e70d2d19a32a4531d11621e7e08794e974e98
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-11 03:59:31 +0200

    Update privatesubnet1.tf

--- infra/network/privatesubnet1.tf
+++ infra/network/privatesubnet1.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "subnet_one" {
+resource "aws_subnet" "private_subnet_one" {
   vpc_id     = aws_vpc.my_vpc.id
   cidr_block = var.private_subnet_one_cidr
   availability_zone = var.az1
