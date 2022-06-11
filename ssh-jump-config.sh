239f0d568ba060e2ef3b029b431c7f6c207f0f82
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-11 22:06:55 +0200

    Create ssh-jump-config.sh

---
+++ ssh-jump-config.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+tee /var/jenkins_home/.ssh/config <<EOF
+
+Host *
+    Port 22
+    StrictHostKeyChecking no
+    UserKnownHostsFile /dev/null
+    ServerAliveInterval 60
+    ServerAliveCountMax 30
+
+Host bastion
+    HostName $1
+    User ubuntu
+    IdentityFile private.pem
+
+Host private
+    HostName $2
+    User ubuntu
+    IdentityFile private.pem
+    ProxyCommand ssh bastion -W %h:%p
+
+EOF
