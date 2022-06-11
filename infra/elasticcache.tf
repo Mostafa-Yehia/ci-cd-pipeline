acf72394eb9d145b88c246e0e8c128fcf72bc073
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-11 04:01:55 +0200

    Update elasticcache.tf

--- infra/elasticcache.tf
+++ infra/elasticcache.tf
@@ -1,5 +1,5 @@
 resource "aws_elasticache_cluster" "my_redis" {
-  cluster_id           = "my_redis"
+  cluster_id           = "my-redis"
   engine               = "redis"
   node_type            = "cache.t2.micro"
   num_cache_nodes      = 1
