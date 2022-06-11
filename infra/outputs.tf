548245bb223d0657a8a3db206249f2a80105a46e
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-11 18:21:23 +0200

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,8 +1,32 @@
-
 output ec2pubip {
     value = aws_instance.ec2a.public_ip
 }
 
 output ec2prvip {
     value = aws_instance.ec2b.private_ip
+}
+
+output rdshost {
+    value = aws_db_instance.my_rds.address
+}
+
+output rdsport {
+    value = aws_db_instance.my_rds.port
+}
+
+output rdsusername {
+    value = aws_db_instance.my_rds.username
+}
+
+output rdspassword {
+    value = module.vars.my_secret
+}
+
+output redishost {
+    value = aws_elasticache_cluster.my_redis.cluster_address
+}
+
+
+output redisport {
+    value = aws_elasticache_cluster.my_redis.port
 }
