5625b767a4e801a554b54a44a36d4af1994f1c9f
Mostafa-Yehia <95745322+Mostafa-Yehia@users.noreply.github.com>
2022-06-12 17:04:22 +0200

    Update automatic-node-creation2.sh

--- automatic-node-creation2.sh
+++ automatic-node-creation2.sh
@@ -3,7 +3,7 @@
 cli_name=$1
 cli_pass=$2
 
-error=`java -jar /var/jenkins_home/jars/jenkins-cli.jar -s http://localhost:8080 -auth ${cli_name}:${cli_pass} get-node private 2> /dev/null | wc -l`
+error=`java -jar /var/jenkins_home/jars/jenkins-cli.jar -s http://localhost:8080 -auth ${cli_name}:${cli_pass} get-node private 2> /dev/null | wc -l || true`
 
 echo "error code: ${error}"
 
